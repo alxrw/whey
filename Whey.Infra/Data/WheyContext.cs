968f3cd71666727a9b24b0565f3f5cdd29b8fe60
yhoundz <43507714+yhoundz@users.noreply.github.com>
2025-12-05 23:32:06 -0600

    added dockerfile + docker-compose

--- Whey.Infra/Data/WheyContext.cs
+++ Whey.Infra/Data/WheyContext.cs
@@ -7,6 +7,7 @@ public class WheyContext : DbContext
 {
 	public DbSet<WheyClient> Clients { get; set; }
 	public DbSet<Package> Packages { get; set; } // tracked packages
+	public DbSet<PackageMetric> PackageMetrics { get; set; } // ??
 	public WheyContext(DbContextOptions<WheyContext> options) : base(options) { }
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
